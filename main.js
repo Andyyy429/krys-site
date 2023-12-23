2bca8fc492890a62e79a22ae8f8e728660fe420a
Andyyy429 <anderson.gibbs@my.uwi.edu>
2023-12-22 23:37:34 -0400

    Site Changes

--- main.js
+++
@@ -1,8 +0,0 @@
-let dropcat = document.querySelector(".dropdown-content");
-function showCatagories() {
-    if (dropcat.style.display === "none") {
-        dropcat.style.display = "block";
-    } else {
-        dropcat.style.display = "none";
-    }
- }
